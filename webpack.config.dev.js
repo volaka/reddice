c35bac75ec2c545e4b4204a325a415828cef5f67
volaka <hasancanvolaka@gmail.com>
2017-02-01 14:54:11 +0300

    react, webpack setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+    devtools: 'eval-source-map',
+    entry: path.join(__dirname, '/client/index.js'),
+    output: {
+        path: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, '/client'),
+                loaders: [ 'babel' ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '', '.js' ]
+    }
+}
