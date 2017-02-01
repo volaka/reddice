ecc86fb1d369b20026df75ce0fa9fdcf2ec3c43d
volaka <hasancanvolaka@gmail.com>
2017-02-01 18:03:28 +0300

    Add server side validations

--- client/components/signup/SignUpForm.js
+++ client/components/signup/SignUpForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import timezones from './../../data/timezones';
 import map from 'lodash/map';
+import classnames from 'classnames';
 
 class SignUpForm extends React.Component {
     constructor(props) {
@@ -10,7 +11,9 @@ class SignUpForm extends React.Component {
             email: '',
             password: '',
             passwordConfirmation: '',
-            timezone: ''
+            timezone: '',
+            errors: {},
+            isLoading: false
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
@@ -24,47 +27,56 @@ class SignUpForm extends React.Component {
 
     onSubmit(e) {
         e.preventDefault();
-        this.props.userSignUpRequest(this.state);
+        this.setState({errors: {}, isLoading: true});
+        this.props.userSignUpRequest(this.state).then(
+            () => {},
+            ({ data }) => this.setState({ errors: data, isLoading: false })
+        );
     }
     render() {
         const options = map(timezones, (value, key) =>
             <option key={value} value={value}>{key}</option>
         );
-
+        const { errors } = this.state;
         return(
             <form onSubmit={this.onSubmit}>
                 <h1>Join</h1>
 
-                <div className="form-group">
+                <div className={classnames("form-group", {'has-error': errors.username})}>
                     <label className="control-label">Username</label>
                     <input type="text" onChange={this.onChange} value={this.state.username} className="form-control" name="username"/>
+                    {errors.username && <span className="help-block">{errors.username}</span>}
                 </div>
 
-                <div className="form-group">
+                <div className={classnames("form-group", {'has-error': errors.email})}>
                     <label className="control-label">Email</label>
                     <input type="text" onChange={this.onChange} value={this.state.email} className="form-control" name="email"/>
+                    {errors.email && <span className="help-block">{errors.email}</span>}
                 </div>
 
-                <div className="form-group">
+                <div className={classnames("form-group", {'has-error': errors.password})}>
                     <label className="control-label">Password</label>
                     <input type="password" onChange={this.onChange} value={this.state.password} className="form-control" name="password"/>
+                    {errors.password && <span className="help-block">{errors.password}</span>}
                 </div>
 
-                <div className="form-group">
+                <div className={classnames("form-group", {'has-error': errors.passwordConfirmation})}>
                     <label className="control-label">Password Confirmation</label>
                     <input type="text" onChange={this.onChange} value={this.state.passwordConfirmation} className="form-control" name="passwordConfirmation"/>
+                    {errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
                 </div>
 
-                <div className="form-group">
+                <div className={classnames("form-group", {'has-error': errors.timezone})}>
                     <label className="control-label">Timezone</label>
                     <select type="text" onChange={this.onChange} value={this.state.timezone} className="form-control" name="timezone">
                         <option value="" disabled> Choose your Timezone</option>
                         {options}
                     </select>
+                    {errors.timezone && <span className="help-block">{errors.timezone}</span>}
                 </div>
 
                 <div className="form-group">
-                    <button className="btn btn-primary btn-lg">Sign Up!</button>
+                    <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">Sign Up!</button>
                 </div>
             </form>
         );
--- server/index.js
+++ server/index.js
@@ -1,26 +1,40 @@
 import express from 'express';
 import path from 'path';
-
+import bodyParser from 'body-parser';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev.js';
+import users from './routes/users';
 
 let app = express();
 
-const compiler = webpack(webpackConfig);
+/**
+ * router middleware setup
+ */
 
+app.use(bodyParser.json());
+app.use('/api/users', users);
+
+/**
+ * webpack express setup
+ */
+
+const compiler = webpack(webpackConfig);
 app.use(webpackMiddleware(compiler, {
     hot: true,
     publicPath: webpackConfig.output.publicPath,
     noInfo: true
 }));
-
 app.use(webpackHotMiddleware(compiler));
 
+/**
+ * routes
+ */
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
+//port listen
 app.listen(3000, () => console.log('Running on localhost:3000'));
